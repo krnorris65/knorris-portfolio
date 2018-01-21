3d967a3ca136c6c9facf2976da4c638898e45625
Kristen Norris <krnorris65@gmail.com>
2018-01-21 14:27:30 -0600

    initial formatting of page

---
+++ app/app.js
@@ -0,0 +1,18 @@
+// angular.module("LifeReelApp", ["ngRoute"])
+
+// angular.module("LifeReelApp").config(function ($routeProvider) {
+// 	/**
+//      * Configure all Angular application routes here
+//      */
+// 	$routeProvider.
+// 		when("/auth", {
+// 			templateUrl: "app/auth/partials/auth.html",
+// 			controller: "AuthCtrl"
+// 		})
+// 		.when("/register", {
+// 			templateUrl: "app/auth/partials/register.html",
+// 			controller: "AuthCtrl"
+// 		})
+// 		.otherwise("/auth")
+
+// })
