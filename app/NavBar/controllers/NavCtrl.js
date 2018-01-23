ec1cc50f831e80173200609ee5deea5d5776631a
Kristen Norris <krnorris65@gmail.com>
2018-01-23 12:02:35 -0600

    set up structure for parallax

--- app/NavBar/controllers/NavCtrl.js
+++ app/NavBar/controllers/NavCtrl.js
@@ -1,9 +1,12 @@
 angular.module("KNPortfolio").controller("NavCtrl",
     function ($scope) {
-        
+
         // gives functionality to collapsed menu 
         $(".button-collapse").sideNav();
 
+        $(document).ready(function(){
+            $('.parallax').parallax();
+          });
 
     }
 )
