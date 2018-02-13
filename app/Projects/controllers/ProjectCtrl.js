42f9d47fb37138cd8bd673ec754c2e13caf9156b
Kristen Norris <krnorris65@gmail.com>
2018-02-13 14:01:42 -0600

    fixing image links

--- app/Projects/controllers/ProjectCtrl.js
+++ app/Projects/controllers/ProjectCtrl.js
@@ -4,7 +4,7 @@ angular.module("KNPortfolio").controller("ProjectCtrl",
         $scope.Project1 = "Life Reel"
         $scope.Link1 = "https://life-reel.firebaseapp.com"
         $scope.GitHub1 = "https://github.com/krnorris65/front-end-capstone"
-        $scope.Image1 = "../../../img/LifeReelApp.png" 
+        $scope.Image1 = "img/LifeReelApp.png" 
         $scope.AltText1 = "Life Reel Frontend Capstone"
         $scope.Description1 = "Created with AngularJS, HTML and CSS, Life Reel allows users to track major life events as they happen so they are able to easily look back and remember the important things that happened throughout the year."
 
