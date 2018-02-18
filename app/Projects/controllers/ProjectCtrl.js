1077474bc52a005e5bf8d29b31ea733a2b818ac5
Kristen Norris <krnorris65@gmail.com>
2018-02-17 20:44:57 -0600

    updated project description for life reel

--- app/Projects/controllers/ProjectCtrl.js
+++ app/Projects/controllers/ProjectCtrl.js
@@ -6,7 +6,7 @@ angular.module("KNPortfolio").controller("ProjectCtrl",
         $scope.GitHub1 = "https://github.com/krnorris65/front-end-capstone"
         $scope.Image1 = "img/LifeReelApp.png" 
         $scope.AltText1 = "Life Reel Frontend Capstone"
-        $scope.Description1 = "Created with AngularJS, HTML and CSS, Life Reel allows users to track major life events as they happen so they are able to easily look back and remember the important things that happened throughout the year."
+        $scope.Description1 = "Web application to track major life events as they happen. Life Reel makes it easy for users to look back and remember the important things that happened throughout the year as well as interact with other users on the application. Created with AngularJS, HTML and CSS, Life Reel is a complete CRUD application that uses Firebase Authentication and Realtime Database."
 
         // $scope.Project2 = "Backend Capstone"
 
